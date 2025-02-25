af9a70e9cb9f2530b444e1a63339ee5dab511215
wopgan <sylviolimas@gmail.com>
2025-02-25 14:10:32 -0300

    Feito ajustes no CSS do painel, tamanho do painel, posição do menu hamburguer e posição do brand logo

---
+++ templates/static/js/navbar.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menuToggle");
+    const navMenu = document.getElementById("navMenu");
+
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("show");
+    });
+});
